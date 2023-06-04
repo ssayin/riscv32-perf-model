764cfe49274572cc239e9949d67b4b4e85f7a493
Serdar Sayın <serdarsayin@pm.me>
2023-06-05 01:26:48 +0300

    import json, spdlog, decoder

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,13 @@
-int main(int argc, char** argv) { return 0; }
+#include <bitset>
+
+#include "common/common.hpp"
+#include "nlohmann/json.hpp"
+#include "spdlog/spdlog.h"
+
+int main(int argc, char **argv) {
+  spdlog::info("Starting model");
+
+  spdlog::info(std::bitset<32>{masks::sign_bit}.to_string());
+
+  return 0;
+}
